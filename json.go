7f8894cf24efb90a996ee85325db71eef9a2a97c
ArashZich <arashzich1992@gmail.com>
2024-01-22 12:40:05 +0330

    boilerplate for http server

---
+++ json.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"encoding/json"
+	"log"
+	"net/http"
+)
+
+func respondWithError(w http.ResponseWriter, code int, message string) {
+
+	if code > 499 {
+		log.Println("Responding with 5XX error:", message)
+	}
+
+	type errResponse struct {
+		Error string `json:"error"`
+	}
+
+	responseWithJSON(w, code, errResponse{Error: message})
+}
+
+func responseWithJSON(w http.ResponseWriter, code int, payload interface{}) {
+	dat, err := json.Marshal(payload)
+
+	if err != nil {
+		log.Printf("Failed to marshal payload to JSON response: %v", payload)
+		w.WriteHeader(500)
+		return
+	}
+
+	w.Header().Add("Content-Type", "application/json")
+	w.WriteHeader(code)
+	w.Write(dat)
+}
